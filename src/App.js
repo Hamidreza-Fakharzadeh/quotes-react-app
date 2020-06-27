92da7c524faa5ffb25ca499521e0abada2ea6ec0
hamidreza <hrfarzadeh@gmail.com>
2020-06-27 20:49:42 +0100

    set api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import './App.css';
 
 function App () {
-  // const [quotes, setQuotes] = useState ({});
-  // useEffect (() => generateQuotes (), []);
-  // function generateQuotes () {
-  //   fetch ('https://arcane-brushlands-06771.herokuapp.com/quotes')
-  //     .then (response => response.json ())
-  //     .then (data => setQuotes (data));
-  //   console.log (quotes);
-  // }
+  const [quotes, setQuotes] = useState ({});
+  useEffect (() => generateQuotes (), []);
+  function generateQuotes () {
+    fetch ('https://arcane-brushlands-06771.herokuapp.com/quotes')
+      .then (response => response.json ())
+      .then (data => setQuotes (data));
+    console.log (quotes);
+  }
   return <div className="App" />;
 }
 
