f69c158796d2d8f35ff510e2e0945f370d7e3767
hamidreza <hrfarzadeh@gmail.com>
2020-06-27 23:08:02 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React,{useState, useEffect} from 'react';
 import './App.css';
 
-function App () {
+function App() {
   const [quotes, setQuotes] = useState ("");
-  useEffect (() => generateQuotes (), []);
+  useEffect(() => generateQuotes(), []);
   function generateQuotes () {
-    fetch ('https://arcane-brushlands-06771.herokuapp.com/')
-      .then (response => response.text ())
-      .then (data => setQuotes (data));
-    console.log (quotes);
+    fetch('https://arcane-brushlands-06771.herokuapp.com')
+      .then (response => response.text())
+      .then (data => setQuotes(data));
+    console.log(quotes);
   }
-  return <div className="App" />;
+  return(
+     <div className="App">
+       <p>hi</p>
+     </div>
+      
+     
+  )
 }
 
 export default App;
