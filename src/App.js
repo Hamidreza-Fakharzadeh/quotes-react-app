8ea76ad540bd3e4bc82a37eda2c1dd79f381331c
hamidreza <hrfarzadeh@gmail.com>
2020-06-27 18:12:51 +0100

    comment fetch to test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App () {
-  const [quotes, setQuotes] = useState ({});
-  useEffect (() => generateQuotes (), []);
-  function generateQuotes () {
-    fetch ('https://arcane-brushlands-06771.herokuapp.com/quotes')
-      .then (response => response.json ())
-      .then (data => setQuotes (data));
-    console.log (quotes);
-  }
+  // const [quotes, setQuotes] = useState ({});
+  // useEffect (() => generateQuotes (), []);
+  // function generateQuotes () {
+  //   fetch ('https://arcane-brushlands-06771.herokuapp.com/quotes')
+  //     .then (response => response.json ())
+  //     .then (data => setQuotes (data));
+  //   console.log (quotes);
+  // }
   return <div className="App" />;
 }
 
