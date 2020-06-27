6927b7cf50e2288fa4394dea55935fa54dd2f081
hamidreza <hrfarzadeh@gmail.com>
2020-06-27 17:36:02 +0100

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App () {
   const [quotes, setQuotes] = useState ({});
   useEffect (() => generateQuotes (), []);
   function generateQuotes () {
-    fetch ('https://arcane-brushlands-06771.herokuapp.com/')
+    fetch ('https://arcane-brushlands-06771.herokuapp.com/quotes')
       .then (response => response.json ())
       .then (data => setQuotes (data));
     console.log (quotes);
